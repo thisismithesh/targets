26d01b3b6ef495dd7e6247e71e921085f9639189
thisismithesh <105309316+thisismithesh@users.noreply.github.com>
2026-06-26 00:41:21 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    sourcemap: true, // TEMP: turn off again once the blank-screen bug is fixed
+  },
   server: {
     port: 3000,
     open: true,
